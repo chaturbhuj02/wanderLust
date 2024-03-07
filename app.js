55a3c1bb4682b71f472d78167f2074a0a79beb03
chaturbhuj02 <chaturbhuj02@gmail.com>
2024-03-07 18:41:40 +1100

    inital commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+main()
+.then(()=>{
+    console.log("Connected to DB");
+}).catch((err)=>{
+    console.log("Error:", err)
+});
+
+async function main(){
+    await mongoose.connect(MONGO_URL);
+}
+
+
+
+app.get("/", (req, res)=>{
+    res.send("Hi, I am groot");
+})
+
+app.listen(8080, ()=>{
+    console.log("Server is listening to port 8080");
+})
